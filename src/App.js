edc6cb5287c761819c80971d046d29a4e2fcb6b5
pguilhermef <pguilhermesantos@hotmail.com>
2022-08-01 15:49:10 -0300

    Criação de rotas e adição de páginas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,57 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <div>
+          <p>TrybeTunes</p>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              component={ Login }
+            />
+            <Route
+              exact
+              path="/search"
+              component={ Search }
+            />
+            <Route
+              exact
+              path="/album/:id"
+              component={ Album }
+            />
+            <Route
+              exact
+              path="/favorites"
+              component={ Favorites }
+            />
+            <Route
+              exact
+              path="/profile"
+              component={ Profile }
+            />
+            <Route
+              exact
+              path="/profile/edit"
+              component={ ProfileEdit }
+            />
+            <Route
+              component={ NotFound }
+            />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
   }
 }
 
