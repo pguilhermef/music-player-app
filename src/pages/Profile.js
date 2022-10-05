2129aedb7c3e3105a61c3bd3dba8d0309a4d295f
pguilhermef <pguilhermesantos@hotmail.com>
2022-10-05 12:08:45 -0300

    feat: :sparkles: Cria aba de perfil

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
-class Profile extends React.Component {
+class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+      user: {
+        name: '',
+        email: '',
+        image: '',
+        description: '',
+      },
+    };
+  }
+
+  async componentDidMount() {
+    const user = await getUser();
+    this.setState({ user }, () => this.loadingState());
+  }
+
+  loadingState = () => this.setState(({ loading }) => ({ loading: !loading }));
+
   render() {
+    const { loading, user } = this.state;
+    const { name, email, image, description } = user;
     return (
       <div data-testid="page-profile">
-        <h1>Profile</h1>
         <Header />
+        {loading ? <Loading /> : (
+          <div>
+            <img src={ image } alt={ name } data-testid="profile-image" />
+            <Link to="/profile/edit">Editar perfil</Link>
+            <p>{ name }</p>
+            <p>{ email }</p>
+            <p>{ description }</p>
+          </div>
+        )}
       </div>
     );
   }
